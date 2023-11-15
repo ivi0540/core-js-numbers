ccc83baef374127237cc81b29fb7faa16cf0658f
ivi0540 <ivi0540@gmail.com>
2023-11-15 12:31:37 +0300

    feat: solved getSumOfDigits task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -319,8 +319,13 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  const strNum = String(num);
+  let sum = 0;
+  for (let i = 0; i <= strNum.length - 1; i += 1) {
+    sum += Number(strNum[i]);
+  }
+  return sum;
 }
 
 /**
