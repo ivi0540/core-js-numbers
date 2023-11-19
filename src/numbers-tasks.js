e65460af6117b15607883ed0642fe07d58e31bf8
ivi0540 <ivi0540@gmail.com>
2023-11-19 23:25:14 +0300

    feat: solved getCountOfOddNumbers task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -649,8 +649,15 @@ function getHypotenuse(a, b) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  let count = 0;
+  const newNum = Math.abs(number);
+  for (let i = 0; i <= newNum; i += 1) {
+    if (i % 2 !== 0) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 module.exports = {
